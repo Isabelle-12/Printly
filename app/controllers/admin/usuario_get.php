f4d113beedfaf1d6ee032148c493f8960be662c8
Isabelle Faculdade <isabelleribeirodossantos12@gmail.com>
2026-03-31 19:07:47 -0300

    update usuario get

--- app/controllers/admin/usuario_get.php
+++ app/controllers/admin/usuario_get.php
@@ -7,12 +7,9 @@ include_once(__DIR__ . '/../../../config/conexao.php');
         'data'      => []
     ];
 
-    if(isset($_GET['id'])){
-        $stmt = $conexao->prepare("SELECT * FROM usuarios where id = ?");
-        $stmt->bind_param("i",$_GET['id']);
-    }else{
-        $stmt = $conexao-> prepare("SELECT * FROM usuarios");
-    }
+    $stmt = $conexao->prepare("SELECT * FROM usuarios WHERE tipo_perfil = 'CLIENTE' ORDER BY nome");
+    $stmt->execute();
+    $resultado = $stmt->get_result();
 
     $stmt->execute();
     $resultado = $stmt->get_result();

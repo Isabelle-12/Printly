ac80d67c41d23db741b1af57513ec084b43f3b0d
Paula Mendes Pontes <paulacmp07@gmail.com>
2026-04-08 10:19:10 -0300

    adicionando rotas

--- config/routes.php
+++ config/routes.php
@@ -3,5 +3,9 @@
 return [
     'admin-notificacoes' => __DIR__ . '/../app/views/admin_notificacoes.html',
     'usuario-painel'     => __DIR__ . '/../app/views/usuario_painel.html',
-    'admin-usuarios'     => __DIR__ . '/../app/views/gerenciador_de_usuarios.html'
+    'admin-usuarios'     => __DIR__ . '/../app/views/gerenciador_de_usuarios.html',
+    'cadastro'     => __DIR__ . '/../app/views/cadastro_usuario.html',
+    'login'     => __DIR__ . '/../app/views/login.html',
+    'home'     => __DIR__ . '/../app/views/home.html',
+
 ];
